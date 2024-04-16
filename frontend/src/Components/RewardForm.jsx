d4efa8effeb360698f090f5adbb119b958ad048d
karthi-100 <karthivicky42@gmail.com>
2024-04-17 00:27:43 +0530

    completed task 4 and changes

--- frontend/src/Components/RewardForm.jsx
+++ frontend/src/Components/RewardForm.jsx
@@ -1,5 +1,5 @@
 import React ,{useEffect, useState} from 'react'
-import { Form, Input, InputNumber, Select, Button } from 'antd';
+import { Form, Input, InputNumber, Select, Button, Row,Col } from 'antd';
 
 function RewardForm({data,onClose}) {
   const [initialState,setInitial]=useState(data);
@@ -16,71 +16,76 @@ function RewardForm({data,onClose}) {
     onFinish={onFinish}
     initialValues={data}
   >
-    <Form.Item label="Name" name="name">
-      <Input />
-    </Form.Item>
-
-    <Form.Item label="Earn">
-      <Form.Item
-        label="Purchase Amount"
-        name={['earn', 'purchaseAmount']}
-        rules={[{ required: true, message: 'Please input purchase amount!' }]}
-      >
-        <InputNumber min={0} />
-      </Form.Item>
-      <Form.Item
-        label="Points"
-        name={['earn', 'points']}
-        rules={[{ required: true, message: 'Please input points!' }]}
-      >
-        <InputNumber min={0} />
-      </Form.Item>
-      <Form.Item
-        label="Referral Points"
-        name={['earn', 'referralPoints']}
-        rules={[{ required: true, message: 'Please input referral points!' }]}
-      >
-        <InputNumber min={0} />
-      </Form.Item>
-    </Form.Item>
-
-    <Form.Item label="Redeem">
-      <Form.Item
-        label="Redeem Points"
-        name={['redeem', 'redeemPoints']}
-        rules={[{ required: true, message: 'Please input redeem points!' }]}
-      >
-        <InputNumber min={0} />
-      </Form.Item>
-      <Form.Item
-        label="Cash Value"
-        name={['redeem', 'cashValue']}
-        rules={[{ required: true, message: 'Please input cash value!' }]}
-      >
-        <InputNumber min={0} />
-      </Form.Item>
-      <Form.Item
-        label="Minimum Points"
-        name={['redeem', 'minPoints']}
-        rules={[{ required: true, message: 'Please input minimum points!' }]}
-      >
-        <InputNumber min={0} />
-      </Form.Item>
-      <Form.Item
-        label="Display Mode"
-        name={['redeem', 'displayMode']}
-        rules={[{ required: true, message: 'Please select display mode!' }]}
-      >
-        <Select>
-          <Option value="Percentage">Percentage</Option>
-          <Option value="Fixed">Fixed</Option>
-        </Select>
-      </Form.Item>
-    </Form.Item>
-
+    <Row gutter={[16, 16]}>
+      <Col span={24}>
+        <Form.Item label="Name" name="name">
+          <Input />
+        </Form.Item>
+      </Col>
+      <Col span={12}>
+        <Form.Item label="Earn">
+          <Form.Item
+            label="Purchase Amount"
+            name={['earn', 'purchaseAmount']}
+            rules={[{ required: true, message: 'Please input purchase amount!' }]}
+          >
+            <InputNumber min={0} />
+          </Form.Item>
+          <Form.Item
+            label="Points"
+            name={['earn', 'points']}
+            rules={[{ required: true, message: 'Please input points!' }]}
+          >
+            <InputNumber min={0} />
+          </Form.Item>
+          <Form.Item
+            label="Referral Points"
+            name={['earn', 'referralPoints']}
+            rules={[{ required: true, message: 'Please input referral points!' }]}
+          >
+            <InputNumber min={0} />
+          </Form.Item>
+        </Form.Item>
+      </Col>
+      <Col span={12}>
+        <Form.Item label="Redeem">
+          <Form.Item
+            label="Redeem Points"
+            name={['redeem', 'redeemPoints']}
+            rules={[{ required: true, message: 'Please input redeem points!' }]}
+          >
+            <InputNumber min={0} />
+          </Form.Item>
+          <Form.Item
+            label="Cash Value"
+            name={['redeem', 'cashValue']}
+            rules={[{ required: true, message: 'Please input cash value!' }]}
+          >
+            <InputNumber min={0} />
+          </Form.Item>
+          <Form.Item
+            label="Minimum Points"
+            name={['redeem', 'minPoints']}
+            rules={[{ required: true, message: 'Please input minimum points!' }]}
+          >
+            <InputNumber min={0} />
+          </Form.Item>
+          <Form.Item
+            label="Display Mode"
+            name={['redeem', 'displayMode']}
+            rules={[{ required: true, message: 'Please select display mode!' }]}
+          >
+            <Select>
+              <Option value="Percentage">Percentage</Option>
+              <Option value="Fixed">Fixed</Option>
+            </Select>
+          </Form.Item>
+        </Form.Item>
+      </Col>
+    </Row>
     <Form.Item>
       <Button type="primary" htmlType="submit">
-        {Object.keys(data).length?"Update":"Submit"}
+        {Object.keys(data).length ? 'Update' : 'Submit'}
       </Button>
     </Form.Item>
   </Form>
