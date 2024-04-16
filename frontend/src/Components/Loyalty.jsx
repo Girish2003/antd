aba5f8667daf39b201dff117318abed4e3873be8
Girish2003 <cgirish2003@gmail.com>
2024-04-16 19:29:15 +0530

    tabPane update

--- frontend/src/Components/Loyalty.jsx
+++ frontend/src/Components/Loyalty.jsx
@@ -5,27 +5,37 @@ import Trials from '../Tabs/Trials';
 import Rewards from '../Tabs/Rewards';
 import Coupons from '../Tabs/Coupons';
 
-const { TabPane } = Tabs;
+const onChange = (key) => {
+  console.log(key);
+};
 
-function Loyalty() {
-  return (
-    <div style={{ display: 'flex', height: '100%' }}>
-      <Tabs defaultActiveKey="1" onChange={(key) => console.log(key)} tabPosition="left" style={{ flexGrow: 1 }}>
-        <TabPane tab="Promotions" key="1">
-          <Promotions />
-        </TabPane>
-        <TabPane tab="Trials" key="2">
-          <Trials />
-        </TabPane>
-        <TabPane tab="Rewards" key="3">
-          <Rewards />
-        </TabPane>
-        <TabPane tab="Coupons" key="4">
-          <Coupons />
-        </TabPane>
-      </Tabs>
-    </div>
-  );
-}
+const items = [
+  {
+    key: '1',
+    label: 'Promotions',
+    children: <Promotions />,
+  },
+  {
+    key: '2',
+    label: 'Trials',
+    children: <Trials />,
+  },
+  {
+    key: '3',
+    label: 'Rewards',
+    children: <Rewards />,
+  },
+  {
+    key: '4',
+    label: 'Coupons',
+    children: <Coupons />,
+  },
+];
+
+const Loyalty = () => (
+  <div style={{ display: 'flex', height: '100%' }}>
+    <Tabs defaultActiveKey="1" items={items} onChange={onChange} tabPosition="left" style={{ flexGrow: 1 }} />
+  </div>
+);
 
 export default Loyalty;
